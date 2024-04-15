a9df89110e54f8475a26963e82ed56014b9d2060
theo-baccam <theo.baccam@laplateforme.io>
2024-04-15 16:11:04 +0200

    jour01/job09: fix la méthode toSorted, ajouté logs d'appel à la fonction

--- jour01/job09/script.js
+++ jour01/job09/script.js
@@ -1,7 +1,15 @@
 function tri(numbers, order) {
     if (order === "asc") {
-        return numbers.toSorted();
+        return numbers.toSorted(function(a, b) {
+            return a - b
+        });
     } else if (order === "desc") {
-        return numbers.toSorted().toReversed();
+        return numbers.toSorted(function(a, b) {
+            return b - a
+        })
     }
 }
+
+let array = [92, 51, 526, 13, 624]
+console.log(tri(array, "asc"))
+console.log(tri(array, "desc"))
