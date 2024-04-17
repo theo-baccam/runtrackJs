04aa64ea83f8bfbc8c907e17d250708d5b3db517
theo-baccam <theo.baccam@laplateforme.io>
2024-04-17 11:54:38 +0200

    jour03/job02: mélange array d'images et re-affichage

--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -7,14 +7,15 @@ const CORRECT_RAINBOW_ORDER = [
     "arc6.png",
 ]
 
+let remainingImages = []
 
 function loadImages() {
     for (let i = 0; i < CORRECT_RAINBOW_ORDER.length; i++) {
         let imageName = CORRECT_RAINBOW_ORDER[i]
         let tagName = imageName.slice(0, -4)
+        remainingImages.push(imageName)
 
         $("#remainingImages").append(`<img id="${tagName}" src="${imageName}">`);
-        console.log(`#${imageName}`)
 
         $("#remainingImages").on("mouseenter", `#${tagName}`, function() {
             $(`#${tagName}`).css("background-color", "#BBDDFF")
@@ -26,7 +27,47 @@ function loadImages() {
     }
 }
 
+function arrayShuffle(array) {
+    let currentIndex = array.length;
+
+    while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        
+    }
+}
+
+function redisplayRemainingImages() {
+    $("#remainingImages").empty()
+
+    for (let i = 0; i < remainingImages.length; i++) {
+        let imageName = remainingImages[i]
+        let tagName = imageName.slice(0, -4)
+
+        $("#remainingImages").append(`<img id="${tagName}" src="${imageName}">`);
+
+        $("#remainingImages").on("mouseenter", `#${tagName}`, function() {
+            $(`#${tagName}`).css("background-color", "#BBDDFF")
+
+        })
+        $("#remainingImages").on("mouseleave", `#${tagName}`, function() {
+            $(`#${tagName}`).css("background-color", "#FFFFFF")
+        })
+    }
+}
+
+function bindShuffleButton() {
+    $("body").on("click", "#shuffleButton", function() {
+        arrayShuffle(remainingImages)
+        redisplayRemainingImages()
+    })
+}
+
 
 $(document).ready(function() {
     loadImages()
+    bindShuffleButton()
 })
