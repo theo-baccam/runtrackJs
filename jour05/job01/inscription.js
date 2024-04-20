bffad1d53ec205ad642b145a3719e6331d916d69
theo-baccam <theo.baccam@laplateforme.io>
2024-04-20 16:08:08 +0200

    jour05/job01: le reste des vérifications

--- jour05/job01/inscription.js
+++ jour05/job01/inscription.js
@@ -1,5 +1,3 @@
-const SPECIAL_ASCII_CHARACTERS = "!@#$%^&*()_-+=[]{}|;:,.<>?";
-
 function isInputFilled(input) {
     return input.val() !== "";
 }
@@ -31,56 +29,121 @@ function allSignUpInputsFilled() {
 }
 
 function verifyLastName() {
-    const lastName = $("#signUpLastNameInput")
+    const lastName = $("#signUpLastNameInput");
     if (!isInputRequiredLength(lastName, 3)) {
         return `<span class="message">
             Le nom doit faire au moins 3 caractères
         </span>`;
-    }
+    };
 
     return "";
 }
 
 function verifyFirstName() {
-    const firstName = $("#signUpFirstNameInput")
+    const firstName = $("#signUpFirstNameInput");
     if (!isInputRequiredLength(firstName, 3)) {
         return `<span class="message">
             Le prénom doit faire au moins 3 caractères
         </span>`;
-    }
+    };
 
     return "";
 }
 
-function verifyPassWord() {
-    const password = $("#signUpPasswordInput")
+function verifyEmail() {
+    const EMAIL_REGEX =  /([a-zA-Z\d_\-.]+)@([a-zA-Z\d_\-.]+)\.([a-zA-Z]+)/;
+    const email = $("#signUpEmailInput");
+
+    if (email.val().search(EMAIL_REGEX) !== 0) {
+        return `<span class="message">
+            Adresse mail invalide 
+        </span>`;
+    };
+}
+
+function verifyPassword() {
+    const LOWERCASE = /[a-z]/;
+    const UPPERCASE = /[A-Z]/;
+    const DIGITS = /[0-9]/;
+    const SPECIAL_CHARACTERS = (
+        /[!@#\$%\^&\*\(\)_\+\-=\[\]{};':"\\|,\.\/<>?]/
+    );
+
+    const password = $("#signUpPasswordInput");
+
     if (!isInputRequiredLength(password, 8)) {
         return `<span class="message">
             Le mot de passe doit faire au moins 8 caractères
         </span>`;
-    }
+    } else if (password.val().search(LOWERCASE) === -1) {
+        return `<span class="message">
+            Le mot de passe doit inclure au moins une lettre miniscule
+        </span>`;
+    } else if (password.val().search(UPPERCASE) === -1) {
+        return `<span class="message">
+            Le mot de passe doit inclure au moins une lettre majuscule
+        </span>`;
+    } else if (password.val().search(DIGITS) === -1) {
+        return `<span class="message">
+            Le mot de passe doit inclure au moins un chiffre
+        </span>`;
+    } else if (password.val().search(SPECIAL_CHARACTERS) === -1) {
+        return `<span class="message">
+            Le mot de passe doit inclure au moins 1 caractère spécial ASCII 
+        </span>`;
+    };
+
+    return "";
+}
+
+async function verifyAddress() {
+    const address = $("#signUpAddressInput");
+
+    if (!isInputRequiredLength(address, 6)) {
+        return `<span class="message">
+            L'addresse doit faire au moins 6 caractères 
+        </span>`;
+    };
+
+    return "";
+}
+
+async function verifyPostalCode() {
+    const postalCode = $("#signUpPostalCodeInput");
+
+    if (!isInputRequiredLength(postalCode, 2)) {
+        return `<span class="message">
+            Le code postal doit faire au moins 2 caractères 
+        </span>`;
+    };
 
     return "";
 }
 
 
 async function displayMessages() {
-    $(".message").remove()
+    $(".message").remove();
 
-    const allStatus = await allSignUpInputsFilled()
-    const lastNameStatus = await verifyLastName()
-    const firstNameStatus = await verifyFirstName()
-    const passwordStatus = await verifyPassWord()
+    const allStatus = await allSignUpInputsFilled();
+    const lastNameStatus = await verifyLastName();
+    const firstNameStatus = await verifyFirstName();
+    const emailStatus = await verifyEmail();
+    const passwordStatus = await verifyPassword();
+    const addressStatus = await verifyAddress();
+    const postalCodeStatus = await verifyPostalCode();
 
-    $("form").prepend(allStatus)
-    $("#signUpLastNameInput").after(lastNameStatus)
-    $("#signUpFirstNameInput").after(firstNameStatus)
-    $("#signUpPasswordInput").after(passwordStatus)
+    $("form").prepend(allStatus);
+    $("#signUpLastNameInput").after(lastNameStatus);
+    $("#signUpFirstNameInput").after(firstNameStatus);
+    $("#signUpEmailInput").after(emailStatus);
+    $("#signUpPasswordInput").after(passwordStatus);
+    $("#signUpAddressInput").after(addressStatus);
+    $("#signUpPostalCodeInput").after(postalCodeStatus);
 }
 
 $(document).ready(function() {
-    displayMessages()
+    displayMessages();
     $("body").on("input", async function() {
-        displayMessages()
-    })
+        displayMessages();
+    });
 });
